d5bd5738d8910d79bfe444ac96685b2d9a0b2535
gayatribirthare <Yk6i@CAxm4EsjC5>
2023-04-17 12:28:13 -0700

    removing commented code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
-import axios from "axios";
 import Navbar from './Navbar';
-import ShowTweets from "./styles/SignUp.css";
 import "./styles/ShowTweets.css"
 import "./styles/NavBar.css"
 import "./styles/CreateTweet.css"
@@ -10,37 +8,14 @@ import Twitter from "./Twitter";
 import {Link, Route, Routes} from "react-router-dom"
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import User from "./User";
 
-const router = createBrowserRouter([
-  
-  {
-    path: '/login',
-    element: <SignIn />
-  },
-  {
-    path: '/',
-    element: <Twitter />
-  },
-
-
-])
 
 function App() {
-  // const [data, setData] = React.useState([]);
-
-  // React.useEffect(() => {
-  //   axios.get("http://localhost:8000/api/").then((response) => {
-  //     console.log("received response: "+ response);
-  //     setData(response.data.message);
-  //   });
-  // }, []);
 
   return (
     <div className="App">
       <Navbar></Navbar>
-      {/* <RouterProvider router={router } /> */}
       <Routes>
         <Route path="/signin" element={<SignIn/>}></Route>
         <Route path="/signup" element={<SignUp/>}></Route>
@@ -48,7 +23,6 @@ function App() {
         <Route path="/user/:userName" element={<User/>}></Route>
       </Routes>
       <Link to="/" element={<Twitter/>}></Link>
-      {/* <Twitter></Twitter> */}
     </div>
   );
 }
